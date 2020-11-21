f1470de98a2c86bc71a06da1f605884f37de41da
Zak Nesler <zak@nesler.dev>
2020-11-21 16:41:48 -0500

    Clean demo

--- demo.py
+++ demo.py
@@ -1,27 +1,13 @@
-import pygame
 from bibliopixel import LEDStrip
 from bibliopixel.drivers.serial.driver import *
-
-# import the module you'd like to use
 # from BiblioPixelAnimations.strip import Rainbows
 
-# init driver with the type and count of LEDs you're using
-driver = DriverSerial(ledtype=LEDTYPE.NEOPIXEL, num=300, c_order='GRB', device_id=1)
-
-# init controller
-led = LEDStrip(driver)
-
-# init animation; replace with whichever animation you'd like to use
-# anim = Rainbows.RainbowCycle(led)
-
 try:
-    # run the animation
-    # anim.run()
-
-    # while True:
+    driver = DriverSerial(ledtype=LEDTYPE.NEOPIXEL, num=300, c_order='GRB', device_id=1)
+    led = LEDStrip(driver)
+    # anim = Rainbows.RainbowCycle(led)
 
     led.set_brightness(50)
-
     end = 30
 
     while True:
@@ -39,13 +25,8 @@ try:
             led.push_to_driver()
             time.sleep(0.01)
 
-    input("Press the <ENTER> key to continue...")
-
     led.all_off()
     led.push_to_driver()
-
-
 except KeyboardInterrupt:
-    # Ctrl+C will exit the animation and turn the LEDs offs
     led.all_off()
     led.push_to_driver()
